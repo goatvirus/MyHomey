46f9d9fa1c8faa17d113925aa9f4a3cabbb430ff
Jared Lucey <Jared.F.Lucey@gmail.com>
2021-11-25 22:27:46 +0000

    Test

---
+++ README.md
@@ -0,0 +1,20 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/tv.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Mqqt Remote
+Mqtt remote
+
+## About
+Mqtt remote
+
+## Examples
+* "Watch {phrase}"
+
+## Credits
+Jared Lucey
+
+## Category
+**Media**
+Entertainment
+
+## Tags
+#Mqtt
+#Remote
+
---
+++ __init__.py
@@ -0,0 +1,45 @@
+from mycroft import MycroftSkill, intent_handler
+from mycroft.util import LOG
+from adapt.intent import IntentBuilder
+
+class MyHomey(MycroftSkill):
+
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+        @intent_handler(IntentBuilder("")
+            .require("action")
+            .require("device")
+        def handle_action_device(self, message):
+            action = message.data.get("action")
+            device = message.data.get("device")
+            self.log.info(F"Action = {action} , Device = {device}")
+
+def create_skill():
+    return MyHomey()
+
+
+# {action} {item} {channel}
+
+
+# Watch nbc {action:watch} {media:nbc}
+# Turn to nbc
+# Turn on nbc
+# Turn nbc on
+# Switch to nbc
+# Change to nbc
+
+
+# {Action}
+
+# Watch NBC
+# Watch Channel NBC
+# Watch NBC Channel
+# Turn On NBC
+# Turn NBC On
+# Turn on NBC Channel
+# Turn
+
+# Watch TV
+# Turn On TV
+# Turn TV On
