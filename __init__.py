760302ad4e282e4f0efecdbeb5d8840d67f72ccf
Jared Lucey <Jared.F.Lucey@gmail.com>
2021-11-26 20:56:48 +0000

    Test

--- __init__.py
+++ __init__.py
@@ -7,13 +7,19 @@ class MyHomey(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    # TV Action
-    @intent_handler(IntentBuilder('').require('actions').optionally('devices').optionally('functions'))
-    def handle_device_on(self, message):
-        device = message.data.get("devices")
-        function = message.data.get("functions")
-        action = message.data.get("actions")
-        self.log.info(F"Device = {device} , Function = {function} , Action = {action}")
+    # # TV Action
+    # @intent_handler(IntentBuilder('').require('actions').optionally('devices').optionally('functions'))
+    # def handle_device_on(self, message):
+    #     device = message.data.get("devices")
+    #     function = message.data.get("functions")
+    #     action = message.data.get("actions")
+    #     self.log.info(F"Device = {device} , Function = {function} , Action = {action}")
+
+    @intent_handler('turn_on.intent")
+    def handle_turn_on(self, message):
+        device = message.data.get('device')
+        action = "turn on"
+        self.log.info(F"Action = {action} , Device = {device}")
 
 def create_skill():
     return MyHomey()
