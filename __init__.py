db804dce4b440914a6e80611523a8bbda7f3362d
Jared Lucey <Jared.F.Lucey@gmail.com>
2021-11-26 01:07:08 +0000

    Test

--- __init__.py
+++ __init__.py
@@ -13,37 +13,11 @@ class MyHomey(MycroftSkill):
         device = message.data.get("device")
         self.log.info(F"Action = {action} , Device = {device}")
 
-    @intent_handler(IntentBuilder('').require('action_off').require('device'))
-    def handle_device_off(self, message):
-        action = message.data.get("action_off")
-        device = message.data.get("device")
-        self.log.info(F"Action = {action} , Device = {device}")
+    # @intent_handler(IntentBuilder('').require('action_off').require('device'))
+    # def handle_device_off(self, message):
+    #     action = message.data.get("action_off")
+    #     device = message.data.get("device")
+    #     self.log.info(F"Action = {action} , Device = {device}")
 
 def create_skill():
     return MyHomey()
-
-
-# {action} {item} {channel}
-
-
-# Watch nbc {action:watch} {media:nbc}
-# Turn to nbc
-# Turn on nbc
-# Turn nbc on
-# Switch to nbc
-# Change to nbc
-
-
-# {Action}
-
-# Watch NBC
-# Watch Channel NBC
-# Watch NBC Channel
-# Turn On NBC
-# Turn NBC On
-# Turn on NBC Channel
-# Turn
-
-# Watch TV
-# Turn On TV
-# Turn TV On
