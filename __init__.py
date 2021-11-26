6c031706429de2eead162bf59e2bddb69ed9d875
Jared Lucey <Jared.F.Lucey@gmail.com>
2021-11-26 01:18:55 +0000

    Test

--- __init__.py
+++ __init__.py
@@ -11,14 +11,15 @@ class MyHomey(MycroftSkill):
     def handle_device_on(self, message):
         action = message.data.get("action_on")
         device = message.data.get("device")
-        self.log.info(F"Action = {action} , Device = {device}")
+        action_secondary = message.data.get("action_secondary")
+        self.log.info(F"Action = {action} , Secondary Action = {action_secondary} , Device = {device}")
 
     @intent_handler(IntentBuilder('').require('action_off').require('device').optionally('action_secondary'))
     def handle_device_off(self, message):
         action = message.data.get("action_off")
         device = message.data.get("device")
-        self.log.info(F"Action = {action} , Device = {device}")
-
+        action_secondary = message.data.get("action_secondary")
+        self.log.info(F"Action = {action} , Secondary Action = {action_secondary} , Device = {device}")
 
 def create_skill():
     return MyHomey()
